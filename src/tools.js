83b56ee80dfe4964dc982de20becc7094a3c9f25
Ryan Janzen <rpwjanzen@gmail.com>
2014-03-18 17:06:13 -0600

    Initial selection prototype(Broken)

--- src/tools.js
+++ src/tools.js
@@ -2,25 +2,45 @@
 
 
 // chop wood tool
+// TODO
+// click to select region
+// add chop wood jobs for all trees in selected region
 
-function ChopWoodTool() {
-    this._active = true;
-    this._map = null;
+function ChopWoodTool(tiles) {
+    if (!this instanceof ChopWoodTool) {
+        return new ChopWoodTool(tiles);
+    }
+    
+    this._tiles = tiles;
+    this._registerEventHandlers();
 
-    // click to select region
-
-    this.chopTreesInRegion = function(upperLeft, lowerRight) {
-        /*
-        var tilesInRegion = this._map.tilesInRegion(upperLeft, lowerRight);
-        for(var i = 0; i < tilesInRegion.length; i++) {
-            var tile = tilesInRegion[i];
-            var entities = tile.entities;
-            for(var entityIndex = 0; )
-        }
-    */
-    };
+    return this;
 };
 
+ChopWoodTool.prototype._onMouseDown = function(e) {
+    this._firstTile = e;
+};
+
+ChopWoodTool.prototype._onMouseUp = function(e) {
+    this._lastTile = e;
+};
+
+ChopWoodTool.prototype._onMouseOver = function(e) {
+    console.log(this);
+};
+
+ChopWoodTool.prototype._registerEventHandlers = function() {
+    var tiles = this._tiles;
+    for(var x = 0; x < tiles.length; x++) {
+        var row = tiles[x];
+        for(var y = 0; y < row.length; y++) {
+            var tile = row[y];
+            tile.bind('MouseDown', this._onMouseDown.bind(this))
+                .bind('MouseUp', this._onMouseUp.bind(this))
+                .bind('MouseOver', this._onMouseOver);
+        }
+    }
+};
 
 // create stockpile tool
 
