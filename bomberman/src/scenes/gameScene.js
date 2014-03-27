9f10bd3e50a19207cd87df3c723795dc96d1ba2c
Ryan Janzen <rpwjanzen@gmail.com>
2014-03-27 17:07:12 -0600

    Added keyboard control stuff

--- bomberman/src/scenes/gameScene.js
+++ bomberman/src/scenes/gameScene.js
@@ -2,7 +2,17 @@ Crafty.scene('Game',
     function() {
         Crafty.e('TimePerFrame');
 
-        
+        // TODO: Create map
+        var map = [];
+        for(var x = 0; x < 10; x++) {
+        	map[x] = [];
+        	for(var y = 0; y < 10; y++) {
+        		// TODO: New tile?
+        		map[x][y] = null;
+        	}
+        }
+
+        var player = Crafty.c('');
     },
     function() {
     });
---
+++ bomberman/src/systems/keyboardControlSystem.js
@@ -0,0 +1,8 @@
+// keyboard control system
+function KeyboardControlSystem() {
+	this.update = function() {
+		Crafty('KeyboardControlled').each(function(i) {
+			// do something with 'this' entity and current keyboard state?
+		});
+	};
+};
