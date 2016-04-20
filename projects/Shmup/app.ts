c459a8b554af71dc9fa603a176d257bdca7faaf2
Ryan Janzen <ryanj@pandell.com>
2016-04-20 01:14:04 -0600

    get game working

--- projects/Shmup/app.ts
+++ projects/Shmup/app.ts
@@ -5,13 +5,18 @@ import { MainMenu } from "./mainMenu";
 import { Preloader } from "./preloader";
 import { Game } from "./game";
 
-window.onload = function() {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
+export class App {
+  constructor() {
+  }
+  
+  start() {
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
 
-  game.state.add('Boot', new Boot(game));
-  game.state.add('Preloader', new Preloader(game));
-  game.state.add('MainMenu', new MainMenu(game));
-  game.state.add('Game', new Game(game));
+    game.state.add('Boot', new Boot(game));
+    game.state.add('Preloader', new Preloader(game));
+    game.state.add('MainMenu', new MainMenu(game));
+    game.state.add('Game', new Game(game));
 
-  game.state.start('Boot');
+    game.state.start('Boot');
+  }
 };
